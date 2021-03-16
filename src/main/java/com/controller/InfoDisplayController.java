a53023ed6d96607be58714d5e85f33b5ca6fb34b
XIAO_明 <lzm931105@163.com>
2021-03-17 00:16:03 +0800

    #comment 初始化mongoDB

--- src/main/java/com/controller/InfoDisplayController.java
+++ src/main/java/com/controller/InfoDisplayController.java
@@ -7,10 +7,10 @@ import com.pojo.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
 
 
@@ -20,33 +20,57 @@ public class InfoDisplayController {
     private MongoTemplate mongoTemplate;
 
     @PostMapping(value = "getProjectInfo")
-    public String getProjectInfo(@RequestBody PageDto pageDto){
+    public String getProjectInfo(@RequestBody PageDto pageDto) {
         int pageNum = pageDto.getPageNum();
         int pageSize = pageDto.getPageSize();
         //参数判断
-        if(pageNum<0||pageSize<=0){
+        if (pageNum < 0 || pageSize <= 0) {
             return "参数错误";
-        }else{
-            int startNum = pageNum*pageSize;
-            List<Project> projects= mongoTemplate.find(new Query().skip(startNum).limit(pageSize), Project.class);
+        } else {
+            int startNum = pageNum * pageSize;
+            List<Project> projects = mongoTemplate.find(new Query().skip(startNum).limit(pageSize), Project.class);
             OutputFormate outputFormate = new OutputFormate(projects);
             return JSONObject.toJSONString(outputFormate);
         }
     }
 
     @PostMapping(value = "getInvestorInfo")
-    public String getInvestorInfo(@RequestBody PageDto pageDto){
+    public String getInvestorInfo(@RequestBody PageDto pageDto) {
         int pageNum = pageDto.getPageNum();
         int pageSize = pageDto.getPageSize();
         //参数判断
-        if(pageNum<0||pageSize<=0){
+        if (pageNum < 0 || pageSize <= 0) {
             return "参数错误";
-        }else{
-            int startNum = pageNum*pageSize;
-            List<Project> projects= mongoTemplate.find(new Query().skip(startNum).limit(pageSize), Project.class);
+        } else {
+            int startNum = pageNum * pageSize;
+            List<Project> projects = mongoTemplate.find(new Query().skip(startNum).limit(pageSize), Project.class);
             OutputFormate outputFormate = new OutputFormate(projects);
             return JSONObject.toJSONString(outputFormate);
         }
+    }
+
+    /**
+     * 文件上传功能
+     */
+
+    @RequestMapping(value = "uploadfile", produces = {"text/html;charset=UTF-8;"})
+    public String upLoadFile(@RequestParam("uploadLog") MultipartFile file) {
+        if (!file.isEmpty()) {
+            // 获取文件名
+            String fileName = file.getOriginalFilename();
+// 获取文件的后缀名
+            String suffixName = fileName.substring(fileName.lastIndexOf("."));
+            // 文件上传后的路径
+            String filePath = "C:\\Users\\zhangzq\\Desktop\\test\\";
+            File dest = new File(filePath + fileName);
+            // 检测是否存在目录
+            if (!dest.getParentFile().exists()) {
+                dest.getParentFile().mkdirs();
+            }
+
+        }
+        return "";
+
     }
 }
 
