24acd634ac43fe7f3ec74742e037ab47b3f91edc
XIAO_明 <lzm931105@163.com>
2021-03-22 15:14:54 +0800

    #comment 提交项目保存功能

--- src/main/java/com/controller/InfoDisplayController.java
+++ src/main/java/com/controller/InfoDisplayController.java
@@ -3,12 +3,15 @@ package com.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.dto.indto.PageDto;
 import com.dto.outdto.OutputFormate;
+import com.pojo.EntUser;
 import com.pojo.Investor;
 import com.pojo.Project;
+import com.sun.xml.internal.bind.v2.TODO;
 import com.utils.ErrorCode;
 import com.utils.NumGenerate;
 import org.bson.internal.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.FindAndReplaceOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
@@ -118,19 +121,15 @@ public class InfoDisplayController {
     }
 
     /**
-     *
-     *  项目上传（表单）
+     *  查询最新项目草稿
      */
-//    @PostMapping(value = "projectfromupload")
-//    public String projectFormUpload(@RequestBody Project project){
-//        //需要编写项目代码生成器
-//        project.setProjectNo(numGenerate.getNumCode());
-//        mongoTemplate.save(project);
-//        //仅仅返回两个字段
-//        Project outputProject = Project.builder().projectNo(project.getProjectNo()).projectNm(project.getProjectNm()).build();
-//        OutputFormate outputFormate = new OutputFormate(outputProject);
-//        return JSONObject.toJSONString(outputFormate);
-//    }
+    @PostMapping(value = "getdraftbyid")
+    public String projectFormUpload(@RequestBody EntUser entUser){
+        //查询项目草稿
+        Project draftProject = mongoTemplate.findOne(query(where("openId").is(entUser.getOpenId()).and("isDone").is(false)),Project.class);
+        OutputFormate outputFormate = new OutputFormate(draftProject);
+        return JSONObject.toJSONString(outputFormate);
+    }
 
 
 
@@ -139,35 +138,48 @@ public class InfoDisplayController {
      */
     @PostMapping(value = "uploadproject")
     public String upLoadProject(@RequestPart("file") MultipartFile file, Project project) {
-        //需要编写项目代码生成器
-        project.setProjectNo(numGenerate.getNumCode());
-        mongoTemplate.save(project);
-        //文件上传可能会出问题，但是文件
-        if (!file.isEmpty()) {
-            // 获取文件名
-            String fileName = file.getOriginalFilename();
-            // 获取文件的后缀名
-            //String suffixName = fileName.substring(fileName.lastIndexOf("."));
-            // 文件上传后的路径
-            StringBuilder filePathBuffer = new StringBuilder();
-            String filePath = filePathBuffer.append("D:\\").append(project.getOpenId()).append("\\").append(project.getProjectNo()).append("\\").toString();
-            File dest = new File(filePath + fileName);
-            // 检测是否存在目录
-            if (!dest.getParentFile().exists()) {
-                dest.getParentFile().mkdirs();
-            }try {
-                file.transferTo(dest);
-                //文件保存后更新数据库信息
-                project.setBpRoute(filePath);
-            } catch (IllegalStateException e) {
-                return ErrorCode.FILEUPLOADFAILED.toJsonString();
-            } catch (IOException e) {
-                return ErrorCode.FILEUPLOADFAILED.toJsonString();
+        //TODO 如果是保存草稿，则更新草稿箱，并不分配项目id
+        if (!project.getIsDone()) {
+            //查找并替换相应的草稿，如果草稿不存在，则进行插入操作
+            mongoTemplate.update(Project.class)
+                    .matching(query(where("openId").is(project.getOpenId()).and("isDone").is(false)))
+                    .replaceWith(project)
+                    .withOptions(FindAndReplaceOptions.options().upsert())
+                    .findAndReplace();
+            return ErrorCode.SUCCESS.toJsonString();
+        } else {
+            //需要编写项目代码生成器
+            project.setProjectNo(numGenerate.getNumCode());
+            mongoTemplate.save(project);
+            //文件上传可能会出问题
+            if (!file.isEmpty()) {
+                // 获取文件名
+                String fileName = file.getOriginalFilename();
+                // 获取文件的后缀名
+                //String suffixName = fileName.substring(fileName.lastIndexOf("."));
+                // 文件上传后的路径
+                StringBuilder filePathBuffer = new StringBuilder();
+                String filePath = filePathBuffer.append("D:\\").append(project.getOpenId()).append("\\").append(project.getProjectNo()).append("\\").toString();
+                File dest = new File(filePath + fileName);
+                // 检测是否存在目录
+                if (!dest.getParentFile().exists()) {
+                    dest.getParentFile().mkdirs();
+                }
+                try {
+                    file.transferTo(dest);
+                    //文件保存后更新数据库信息
+                    project.setBpRoute(filePath);
+                } catch (IllegalStateException e) {
+                    return ErrorCode.FILEUPLOADFAILED.toJsonString();
+                } catch (IOException e) {
+                    return ErrorCode.FILEUPLOADFAILED.toJsonString();
+                }
             }
-        }
             Project outputProject = Project.builder().projectNo(project.getProjectNo()).projectNm(project.getProjectNm()).build();
+            //TODO 项目信息保存完全后更新EntUser下Project字段
             OutputFormate outputFormate = new OutputFormate(outputProject);
             return JSONObject.toJSONString(outputFormate);
+        }
     }
 }
 
